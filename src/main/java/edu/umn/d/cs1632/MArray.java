5ba51d1db1601220cb54d1ab38eb17c367ed6f9d
rmaclin <rmaclin@d.umn.edu>
2026-09-04 20:29:11 -0500

    Initial Code comment

---
+++ src/main/java/edu/umn/d/cs1632/MArray.java
@@ -0,0 +1,28 @@
+package edu.umn.d.cs1632;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.util.List;
+
+class MArray {
+    // ArrayList<Feature> featureList;
+    // ArrayList<ArrayList<DataItem>> data;
+    private MArray() {}
+    public MArray(String fileName) {
+        try {
+            FileReader filereader = new FileReader(fileName);
+            CSVReader csvReader = new CSVReader(filereader);
+            List<String[]> allData = csvReader.readAll();
+            for (String[] row : allData) {
+                for (String cell : row) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/edu/umn/d/cs1632/Main.java
@@ -0,0 +1,7 @@
+package edu.umn.d.cs1632;
+
+public class Main {
+    public static void main(String[] args) {
+        MArray mArray = new MArray("/Users/rmaclin/Documents/1632/f26/ReadMArray/src/data/simple.csv");
+    }
+}
