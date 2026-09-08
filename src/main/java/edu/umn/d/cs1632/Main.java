4c702e7ffc73e0dd8b9aae68792975d66105211b
rmaclin <rmaclin@d.umn.edu>
2026-09-07 20:32:03 -0500

    Make file path relative rather than absolute

--- src/main/java/edu/umn/d/cs1632/Main.java
+++ src/main/java/edu/umn/d/cs1632/Main.java
@@ -2,6 +2,7 @@ package edu.umn.d.cs1632;
 
 public class Main {
     public static void main(String[] args) {
-        MArray mArray = new MArray("/Users/rmaclin/Documents/1632/f26/ReadMArray/src/data/simple.csv");
+        MArray mArray = new MArray("src/data/simple.csv");
+        // Put your code for queries here
     }
 }
