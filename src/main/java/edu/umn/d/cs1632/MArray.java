113863358d2a6f6dfb0ad7e4b8d5150430f5c4c0
docke102 <docke102@d.umn.edu>
2026-09-11 16:19:34 -0500

    Almost got the Type checker working. Currently getting doubles of some numbers, so may move to a separate file to facilitate return values nad make it a method rather than tightly couping it into the MArray class

--- src/main/java/edu/umn/d/cs1632/MArray.java
+++ src/main/java/edu/umn/d/cs1632/MArray.java
@@ -16,12 +16,26 @@ class MArray {
             List<String[]> allData = csvReader.readAll();
             for (String[] row : allData) {
                 for (String cell : row) {
+                    int stringCheck = 0;
                     try {
                         Integer.valueOf(cell);
                         System.out.print(cell + "(I) \t");
+                        stringCheck += 1;
                     } catch (NumberFormatException e) {
                     //dont stop the program if it doesnt work
                     }
+
+                    try {
+                        Double.valueOf(cell);
+                        System.out.print(cell + "(D) \t");
+                        stringCheck += 1;
+                    } catch (NumberFormatException e) {
+                        //dont stop the program if it doesnt work
+                    }
+                    if (!(stringCheck > 0)){
+                        System.out.print(cell + "(S) \t");
+                    }
+
                 }
                 System.out.println();
             }
