66f98746b1059a7343f8f2cecc6d43f718ed9c70
docke102 <docke102@d.umn.edu>
2026-09-12 16:32:12 -0500

    Create multiple query methods for each command type along with a command checker to see if the users inputs are valid and won't crash the system.

--- src/main/java/edu/umn/d/cs1632/MArray.java
+++ src/main/java/edu/umn/d/cs1632/MArray.java
@@ -9,7 +9,9 @@ import java.util.Scanner;
 class MArray {
     // ArrayList<Feature> featureList;
     // ArrayList<ArrayList<DataItem>> data;
-    private MArray() {}
+    private MArray() {
+    }
+
     public MArray(String fileName) {
         try {
             FileReader filereader = new FileReader(fileName);
@@ -17,9 +19,9 @@ class MArray {
             List<String[]> allData = csvReader.readAll();
             for (String[] row : allData) {
                 for (String cell : row) {
-                    if(StrNumCheck.checkStrNum(cell).equals("(I)")){
+                    if (StrNumCheck.checkStrNum(cell).equals("(I)")) {
                         System.out.print(cell + "(I) \t");
-                    } else if(StrNumCheck.checkStrNum(cell).equals("(D)")){
+                    } else if (StrNumCheck.checkStrNum(cell).equals("(D)")) {
                         System.out.print(cell + "(D) \t");
                     } else {
                         System.out.print(cell + "(S) \t");
@@ -30,29 +32,79 @@ class MArray {
             //Query Function to allow the user
             //to search row data types
             query(allData);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    public void query(List<String[]> allData){
+    public void query(List<String[]> allData) {
         Scanner user = new Scanner(System.in);
         String userInput = " ";
+        int numInt;
+        int numDouble;
+        int numString;
 
-        while (!(userInput.equals("Q"))){
-            System.out.println("Query? ");
+        while (!(userInput.equals("Q"))) {
+            System.out.print("Query? ");
             userInput = user.nextLine();
 
             String[] userCommand = userInput.split(" ");
 
-            if (true){
-
+            switch (userCommand[0]) {
+                case "H" -> queryH(allData, userCommand);
+                case "V" -> queryV(allData, userCommand);
+                case "M" -> queryM(allData, userCommand);
+                default -> System.out.print("Command Not Recognized");
             }
         }
     }
 
+    //Shows the types of arrays in a horizontal line
+    public void queryH(List<String[]> allData, String[] userCommands) {
+        String[] Target = allData.get(Integer.parseInt(userCommands[1]));
+
+        if (StrNumCheck.checkStrNum(userCommands[1]).equals("(I)")) {
+            int targetHorizontal = Integer.parseInt(userCommands[1]);
+
+            for (int i = Integer.parseInt(userCommands[1]); i < allData.size() - 1; i++) {
+                allData.get(0);
+            }
+        }else {
+            System.out.print("Command Not Recognised");
+        }
+    }
+    //Shows the types of data in a Vertical Line
+    public void queryV (List < String[]>allData, String[]userCommands){
+
+    }
+
+    //Shows the types of data in a Submatrix of the original
+    public void queryM (List < String[]>allData, String[]userCommands){
+
+    }
+
+    public boolean checkUserCommand (String[] userCommands) {
+
+        // Checks if the first index of the array is a string
+        if (userCommands[0].equals("H") ||
+                userCommands[0].equals("V") ||
+                userCommands[0].equals("M")) {
+
+            // Checks if the following numbers are
+            // considered Integers
+            for (int i = 1; i < userCommands.length - 1; i++) {
+                if (StrNumCheck.checkStrNum(userCommands[i]).equals(("(I)"))
+                        || userCommands[i] == null) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
 }
+
 
 
